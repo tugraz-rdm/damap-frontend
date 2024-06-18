9179df24480811bf896fe6ff41783f4050db7ecf
ValentinFutterer <e11904654@student.tuwien.ac.at>
2024-06-18 10:59:28 +0200

    Add landing page

--- apps/damap-frontend/src/app/services/config.service.ts
+++ apps/damap-frontend/src/app/services/config.service.ts
@@ -3,8 +3,9 @@ import { Injectable, isDevMode } from '@angular/core';
 
 import { Config } from '@damap/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { lastValueFrom } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -15,22 +16,23 @@ export class ConfigService {
   constructor(
     private http: HttpClient,
     private oauthService: OAuthService,
+    private router: Router,
   ) {}
 
   public initializeApp(): Promise<boolean> {
     return this.loadConfig()
-      .toPromise()
       .then((config: Config) => {
         if (!config) {
           // eslint-disable-next-line no-console
           console.error('Config is missing!');
-          return new Promise<boolean>(_ => false);
+          return new Promise<boolean>(resolve => resolve(false));
         } else {
           this.config = config;
           const authConfig: AuthConfig = {
             issuer: config.authUrl,
             clientId: config.authClient,
             redirectUri: window.location.origin,
+            logoutUrl: window.location.origin,
             oidc: true,
             scope: config.authScope,
             // useSilentRefresh: true,
@@ -40,7 +42,20 @@ export class ConfigService {
           };
           this.oauthService.configure(authConfig);
           this.oauthService.setupAutomaticSilentRefresh();
-          return this.oauthService.loadDiscoveryDocumentAndLogin();
+          return this.oauthService
+            .loadDiscoveryDocumentAndTryLogin()
+            .then(() => {
+              if (
+                this.oauthService.hasValidIdToken() &&
+                this.oauthService.hasValidAccessToken()
+              ) {
+                const url = decodeURIComponent(this.oauthService.state);
+                if (url !== '') {
+                  return this.router.navigateByUrl(url);
+                }
+              }
+              return new Promise<boolean>(resolve => resolve(true));
+            });
         }
       })
       .catch(error => {
@@ -51,7 +66,7 @@ export class ConfigService {
         console.log('Backend: ' + environment.backendurl);
         console.error(error);
         /* eslint-disable no-console */
-        return new Promise(_ => false);
+        return new Promise<boolean>(resolve => resolve(false));
       });
   }
 
@@ -59,8 +74,9 @@ export class ConfigService {
     return this.config.env;
   }
 
-  private loadConfig(): Observable<Config> {
+  private async loadConfig(): Promise<Config> {
     const host = environment.backendurl;
-    return this.http.get<Config>(`${host}config`);
+    const config$ = this.http.get<Config>(`${host}config`);
+    return await lastValueFrom(config$);
   }
 }
