2266e51e07d395c73604794f41a4745b4493317c
ValentinFutterer <e11904654@student.tuwien.ac.at>
2024-11-05 09:07:42 +0100

    Config: Add title environment variable for html title tag

--- apps/damap-frontend/src/app/components/landing-page/landing-page.component.ts
+++ apps/damap-frontend/src/app/components/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { MatAnchor, MatButtonModule } from '@angular/material/button';
@@ -19,7 +19,7 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css',
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
   constructor(private translate: TranslateService) {}
 
   ngOnInit(): void {
